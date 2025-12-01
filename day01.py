4ce38ae02650580d36655b6f0feea4c67dbd0365
Zacharias Jensen <zacharias.jensen@vattenfall.com>
2025-12-01 16:13:10 +0100

    Day 1

--- day01.py
+++ day01.py
@@ -1,20 +1,44 @@
 """Advent of Code - Day 1"""
 
-from lib.utils import read_input
+from lib.utils import *
 
 
 def part1(data):
-    """Solve part 1."""
-    pass
+    position = 50
+    zeros = 0
+    for d in data:
+        dir = d[:1]
+        steps = int(d[1:])
+        if dir == 'L':
+            position -= steps
+        else:
+            position += steps
+        if position % 100 == 0:
+            zeros += 1
+    
+    return zeros
 
 
 def part2(data):
-    """Solve part 2."""
-    pass
+    position = 50
+    zeros = 0
+    for d in data:
+        dir = d[:1]
+        steps = int(d[1:])
+        for s in range(steps):
+            if dir == 'L':
+                position -= 1
+            else:
+                position += 1
+            if position % 100 == 0:
+                zeros += 1
+    
+    return zeros
+    
 
 
 if __name__ == "__main__":
-    data = read_input("inputs/day01.txt")
+    data = read_lines("inputs/day01.txt")
     
     result1 = part1(data)
     print(f"Part 1: {result1}")
