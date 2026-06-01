91b0530af65fe56aa07b980396887f00637ecbeb
Zacharias Jensen <zacharias.jensen@vattenfall.com>
2026-06-01 11:25:24 +0200

    Modified day 3

--- day03.py
+++ day03.py
@@ -1,20 +1,36 @@
 """Advent of Code - Day 3"""
 
-from lib.utils import read_input
+from lib.utils import *
 
 
 def part1(data):
-    """Solve part 1."""
-    pass
+    total = 0
+    for line in data:
+        max1 = max(line[:-1])
+        index1 = line.index(max1)
+        max2 = max(line[index1+1:])
+        jolt = int(line[index1] + max2)
+        total += jolt
+
+    return total
 
 
 def part2(data):
-    """Solve part 2."""
-    pass
+    total = 0
+    for line in data:
+        range_max = len(line) - 12
+        local_max = 0
+        for i in range(0, range_max):
+            val = int(line[i:i+12])
+            if val > local_max:
+                local_max = val
+        total += local_max
+        
+    return total
 
 
 if __name__ == "__main__":
-    data = read_input("inputs/day03.txt")
+    data = read_lines("inputs/day03.txt")
     
     result1 = part1(data)
     print(f"Part 1: {result1}")
